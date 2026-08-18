03bf6c65540f9a06fc3a7cb41fe9ca02898512e2
MuzamilWazir <wazirmuhammadmuzammil@gmail.com>
2026-08-18 13:48:11 +0500

    feat: implement login functionality with user authentication

--- backend/crud/user.py
+++ backend/crud/user.py
@@ -18,3 +18,18 @@ def create_user(email : str , password : str, db : Session):
    db.commit()
    db.refresh(new_user)
    return new_user
+
+
+def login_user(email : str , password : str, db : Session):
+   user_exist = db.query(User).filter(User.email == email).first()
+   if not user_exist:
+        raise HTTPException(401, detail =  "User not existed")
+
+   
+   hash_pw = user_exist.hashed_password 
+   correctPassword = verify_password(password , hash_pw)
+   if not correctPassword:
+        raise HTTPException(401, detail =  "Invalid password")
+
+    
+
