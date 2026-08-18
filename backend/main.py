b13b77fe8be375d8d1123e3f82c58d7f653d749b
MuzamilWazir <wazirmuhammadmuzammil@gmail.com>
2026-08-18 14:16:17 +0500

    feat: include authentication router in main application

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database import base , engine ,test_connection
 from routers.company import router
+from routers.auth import router as auth_router
 
 
 
@@ -10,6 +11,7 @@ test_connection()
 app = FastAPI()
 
 app.include_router(router, prefix="/company", tags=["Company"])
+app.include_router(auth_router, tags=["Authentication"])
 
 @app.get("/")
 def read_root():
